1ed94ef73a2d8fa04986c3d5da80e3e785958796
aaron <quynh.dang@bitfinex.com>
2025-11-04 16:05:36 +0700

    add repro component

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -1,45 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { TextInput, View } from 'react-native'
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+export default function HomeScreen() {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+    <View style={{ flex: 1, paddingTop: 50, backgroundColor: '#FFF' }}>
+      <View style={{ flexDirection: 'row', gap: 8 }}>
+        <TextInput
+          value={
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit morbi nisi aptent condimentum nisi fringilla rutrum sociis senectus ac. Torquent mauris risus ultricies aenean vitae erat mollis congue dui mus'
+          }
+          placeholder='Enter your text here'
+          numberOfLines={1}
+          multiline={false}
+          style={{
+            borderWidth: 1,
+            borderColor: 'black',
+            borderRadius: 5,
+            alignSelf: 'stretch',
+            writingDirection: 'rtl',
+            paddingHorizontal: 2,
+            height: 40
+          }}
+        />
+      </View>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
